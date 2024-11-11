87016ceeddf68348b4fd48a2133e30d8eb826d93
sphoortimpatil <sphoortimpatil@gmail.com>
2024-11-11 11:17:44 +0530

    Initial Commit

---
+++ Readly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Readly
+//
+//  Created by Sphoorti Patil on 11/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
